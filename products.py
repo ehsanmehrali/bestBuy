3a556e05b7ed3d080fdeb5896c194f75125d25d1
ehsanmehrali428@gmail.com <ehsanmehrali428@gmail.com>
2025-05-07 21:37:27 +0200

    Product class was created and tested.

---
+++ products.py
@@ -0,0 +1,61 @@
+
+
+class Product:
+
+    def __init__(self, name: str, price: float, quantity: int):
+        if not name or price < 0 or quantity < 0:
+            raise ValueError("ValueError")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self) -> int:
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.active = False
+
+
+    def is_active(self) -> bool:
+        return self.active == True
+
+    def active(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self) -> str:
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
+    def buy(self, quantity) -> float:
+        if self.active and 0 < quantity <= self.quantity:
+            self.quantity -= quantity
+            self.set_quantity(self.quantity)
+            total_price = quantity * self.price
+            return total_price
+        else:
+            return f"The number of orders({quantity}) is greater than the quantity of '{self.name} in stock({self.quantity})."
+
+def main():
+    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=40)
+    mac = Product("MacBook Air M2", price=1450, quantity=100)
+    try:
+        print(bose.buy(50))
+        print(mac.buy(100))
+        print(mac.is_active())
+
+
+        print(bose.show())
+        print(mac.show())
+
+        bose.set_quantity(1000)
+        print(bose.show())
+    except ValueError as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
